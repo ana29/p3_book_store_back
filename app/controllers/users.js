13212bf934c17e267afafb70764314480a8b1f9d
Ana <ana.godoy@ccc.ufcg.edu.br>
2018-09-04 22:53:47 -0300

    Adding API functions

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -35,6 +35,37 @@ module.exports = function (app) {
       }
     );
   };
+
+
+  controller.getUserById = function (req, res) {
+    console.log('API: getUserById');
+    var _id = req.params.id;
+    User.findById(_id).exec()
+      .then(
+        function (user) {
+          if (!user) throw new Error("User not found");
+          res.json(user)
+        },
+        function (erro) {
+          console.log(erro);
+          res.status(404).json(erro)
+        }
+      );
+  };
+
+  controller.deleteUserById = (req, res) => {
+    console.log('API: deleteUserById');
+    let _id = req.params.id;
+    Usuario.remove(_id).exec()
+      .then(
+        function () {
+          res.end();
+        },
+        function (erro) {
+          return console.error(erro);
+        }
+      );
+  };
   return controller;
 
 }
--- app/routes/users.js
+++ app/routes/users.js
@@ -1,15 +1,17 @@
 module.exports = function (app) {
     let controller = app.controllers.users;
 
-
-
     app.get('/', function (req, res) {
         res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify({ a: 1 }));
+        res.send(":3");
     });
 
     app.route('/api/users')
         .post(controller.saveUser)
         .get(controller.getUsers);
+
+    app.route('/api/users/:id')
+        .get(controller.getUserById)
+        .delete(controller.deleteUserById);
 
 }
