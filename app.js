cd4358c12090669c0a86c967850267568270806e
Ana <ana.godoy@ccc.ufcg.edu.br>
2018-09-04 23:19:48 -0300

    ...

--- app.js
+++ app.js
@@ -8,3 +8,4 @@ http.createServer(app).listen(app.get('port'), function () {
   console.log('Server listening on port ' +
     app.get('port'));
 });
+
--- config/express.js
+++ config/express.js
@@ -22,5 +22,6 @@ module.exports = function () {
     .then('routes')
     .into(app);
 
+
   return app;
 };
